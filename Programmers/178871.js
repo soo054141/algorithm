7ef2738b9210ec964e44364b1ad782385c0b959a
soo054141 <soo054141@gmail.com>
2023-06-18 23:58:37 +0900

    [solve] 달리기 경주

---
+++ Programmers/178871.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+  const result = [...players];
+  const playersMap = new Map();
+
+  for (let i = 0; i < players.length; i++) {
+    playersMap.set(players[i], i);
+  }
+
+  for (const call of callings) {
+    const next = playersMap.get(call) - 1; // 추월 등수
+    playersMap.set(call, next);
+    const target = result[next]; // 바뀌는 놈
+    playersMap.set(target, next + 1);
+    result[next] = call;
+    result[next + 1] = target;
+  }
+
+  return result;
+}
