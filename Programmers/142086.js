960b493102775bc0fb311f717657ece6e6e57d91
Kim Yurim <soo054141@gmail.com>
2023-06-20 03:25:57 +0900

    [solve] 가장 가까운 같은 글자

---
+++ Programmers/142086.js
@@ -0,0 +1,18 @@
+//나의 풀이
+function solution(s) {
+    const myMap = new Map();
+    const result = Array.from({length:s.length}).fill(-1);
+    
+    s.split("").forEach((v, i)=>{
+        if(myMap.has(v)){
+            const prevIdx = myMap.get(v);
+            const getIdx = i - prevIdx;
+            result[i] = getIdx;
+        }
+        myMap.set(v,i)
+    })
+    
+    return result;
+}
+
+//다른사람의 풀이
