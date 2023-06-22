13bb9c6cb3f05246b14ebd1c990aa51947640d8e
Kim Yurim <soo054141@gmail.com>
2023-06-22 23:58:05 +0900

    [fail] 명예의 전당(1).js

--- Programmers/명예의 전당(1).js
+++ Programmers/명예의 전당(1).js
@@ -1,8 +1,7 @@
 function solution(k, score){
     const topRank = Array(k).fill(0).map((_,i)=>score[i]).sort((a,b)=>a-b);
     const result = Array(k).fill().map(v => Math.min(...topRank))
-    
-    console.log(topRank, result)
+
     for(let i = k; i < score.length; i++){
         if(topRank[0] < score[i]){
             topRank.push(score[i]);
