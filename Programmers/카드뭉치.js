f11aab95071b1828c53148afa4f4fbfb4fa3d5aa
Kim Yurim <soo054141@gmail.com>
2023-07-10 01:16:07 +0900

    [solve] 카드뭉치.js
    다시 푼 풀이 추가

--- Programmers/카드뭉치.js
+++ Programmers/카드뭉치.js
@@ -1,18 +1,31 @@
 function solution(cards1, cards2, goal) {
-    let result = true;
-    let idx = 0;
-    
-    while(goal.length !== idx){
-        if(cards1[0] === goal[idx]){
+    // 다시 푼 풀이
+    for(const i of goal){
+        if(cards1[0] === i){
             cards1.shift();
-            idx++;
-        } else if(cards2[0] === goal[idx]){
+        } else if(cards2[0] === i){
             cards2.shift();
-            idx++;
         } else {
-            result = !result;
-            break;
+            return "No";
         }
     }
-    return result ? "Yes" : "No"
+    return "Yes";
+
+    // 첫 제출 풀이
+    // let result = true;
+    // let idx = 0;
+    
+    // while(goal.length !== idx){
+    //     if(cards1[0] === goal[idx]){
+    //         cards1.shift();
+    //         idx++;
+    //     } else if(cards2[0] === goal[idx]){
+    //         cards2.shift();
+    //         idx++;
+    //     } else {
+    //         result = !result;
+    //         break;
+    //     }
+    // }
+    // return result ? "Yes" : "No"
 }
