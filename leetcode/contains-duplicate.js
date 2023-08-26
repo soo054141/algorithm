b36afacaf9d3a5e2f1ed5eb7cc4a09a566caf433
Kim Yurim <soo054141@gmail.com>
2023-08-26 22:50:43 +0900

    Update contains-duplicate.js

--- leetcode/contains-duplicate.js
+++ leetcode/contains-duplicate.js
@@ -1,5 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numSet = new Set(nums);
+    return numSet.size !== nums.length
+};
 
-처음 푼 풀이
+// 처음 푼 풀이
 // var containsDuplicate = function(nums) {
 //     const numMap = new Set();
 
