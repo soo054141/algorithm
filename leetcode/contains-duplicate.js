7a8e8dab13e150ad81752ed2cecae085cb6ae0e3
Kim Yurim <soo054141@gmail.com>
2023-08-26 22:50:19 +0900

    Create contains-duplicate.js

---
+++ leetcode/contains-duplicate.js
@@ -0,0 +1,14 @@
+
+처음 푼 풀이
+// var containsDuplicate = function(nums) {
+//     const numMap = new Set();
+
+//     for(let i = 0; i < nums.length; i++){
+//         if(numMap.has(nums[i])){
+//             return true;
+//         } else {
+//             numMap.add(nums[i])
+//         }
+//     }
+//     return false;
+// };
